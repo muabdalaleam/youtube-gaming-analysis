5ee835fb8728baa9ad50a51ae945e3afbcb8eb9d
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2023-04-24 21:40:05 +0200

    Some CSS

--- Data analysis/.ipynb_checkpoints/dashboard-checkpoint.py
+++ Data analysis/.ipynb_checkpoints/dashboard-checkpoint.py
@@ -1,9 +1,28 @@
-import streamlit as st
-import plotly
+from dash import Dash, html, dcc
+import plotly.express as px
+import plotly.io as pio
+from datetime import datetime
+import pandas as pd
+
+channels_data_scatter_matrix = pio.read_json("plots/json/channels_data_scatter_matrix.json")
+channels_subs_growth = pio.read_json("plots/json/channels_subs_growth_per_time.json")
+subs_vs_channel_name_len = pio.read_json("plots/json/subs_vs_channel_name_len_line_chart.json")
+total_subs_vs_start_date = pio.read_json("plots/json/total_subs_for_channels_per_start_date.json")
+channel_subs_per_country = pio.read_json("plots/json/channels_subs_per_country.json")
+
+app = Dash(__name__,
+           external_stylesheets= "style.css")
 
 
-fig = plotly.io.read_json("plots/json/channels_data_scatter_matrix.json")
+app.layout = html.Div(children=[
+    html.H1(children= "Youtube gaming analysis"),
 
-st.title("Channels data dashboard ")
-st.sidebar.title("navigation")
-st.plotly_chart(fig)
+    html.Div(children='''
+         '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure= channel_subs_per_country)])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
