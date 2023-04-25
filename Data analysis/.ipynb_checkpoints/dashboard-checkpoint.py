358450d2b5b6e07958baeb4cc1d342c00772a5fd
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2023-04-25 11:34:43 +0200

    Bunch of dashboard edits and update data files again

--- Data analysis/.ipynb_checkpoints/dashboard-checkpoint.py
+++ Data analysis/.ipynb_checkpoints/dashboard-checkpoint.py
@@ -1,5 +1,6 @@
 from dash import Dash, html, dcc
 import plotly.express as px
+#include <>
 import plotly.io as pio
 from datetime import datetime
 import pandas as pd
@@ -8,7 +9,9 @@ channels_data_scatter_matrix = pio.read_json("plots/json/channels_data_scatter_m
 channels_subs_growth = pio.read_json("plots/json/channels_subs_growth_per_time.json")
 subs_vs_channel_name_len = pio.read_json("plots/json/subs_vs_channel_name_len_line_chart.json")
 total_subs_vs_start_date = pio.read_json("plots/json/total_subs_for_channels_per_start_date.json")
+youtubers_with_social_accounts = pio.read_json("plots/json/youtubers_count_with_social_accounts.json")
 channel_subs_per_country = pio.read_json("plots/json/channels_subs_per_country.json")
+
 
 EXERNAL_STYLESHEETS = ['assets/style.css']
 
@@ -17,11 +20,42 @@ app = Dash(__name__,
 
 
 app.layout = html.Div(children= [
-    html.Center(children= html.H2(children= "## Youtube gaming analysis")),
-    dcc.markdown("""this is sparta !!""")
+    
+    html.Center(children= html.H1(children= [html.Span(
+        "Youtube", style= {"color": "red"}), " Gaming Analysis"])),
+    
     dcc.Graph(
-        id='example-graph',
-        figure= channel_subs_per_country)])
+        id='channel_subs_per_country',
+        figure= channel_subs_per_country.update_layout(width= 610, height= 500),
+        style= {'position': 'absolute', 'border': '2px solid #fc0303','left': '10px',
+                'display': 'inline-block'}),
+    
+    dcc.Graph(
+        id= 'total_subs_vs_start_date',
+        figure= total_subs_vs_start_date.update_layout(width= 1230),
+        style= {'position': 'absolute', 'left': '10px', 'top': '600px', 'border': '2px solid #fc0303',
+                'display': 'inline-block'}),
+
+    dcc.Graph(
+        id= 'youtubers_with_social_accounts',
+        figure= youtubers_with_social_accounts.update_layout(width= 610, height= 500),
+        style= {'position': 'absolute','right': '10px', 'border': '2px solid #fc0303',
+                'display': 'inline-block'}),
+
+    dcc.Graph(
+        id= 'channels_subs_growth',
+        figure= channels_subs_growth.update_layout(width= 1230,
+                                                   xaxis= dict(rangeslider= dict(
+                                                   visible= True))),
+        style= {'position': 'absolute','left': '10px', 'border': '2px solid #fc0303', 'top': '1020px',
+                'display': 'inline-block'})])
+
+@app.callback(
+    Output('output-container-range-slider', 'children'),
+    [Input('my-range-slider', 'value')])
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- Data analysis/dashboard.py
+++ Data analysis/dashboard.py
@@ -1,14 +1,19 @@
 from dash import Dash, html, dcc
 import plotly.express as px
+#include <>
 import plotly.io as pio
 from datetime import datetime
 import pandas as pd
+
+stacked_channels = pd.read_pickle("../Data en")
 
 channels_data_scatter_matrix = pio.read_json("plots/json/channels_data_scatter_matrix.json")
 channels_subs_growth = pio.read_json("plots/json/channels_subs_growth_per_time.json")
 subs_vs_channel_name_len = pio.read_json("plots/json/subs_vs_channel_name_len_line_chart.json")
 total_subs_vs_start_date = pio.read_json("plots/json/total_subs_for_channels_per_start_date.json")
+youtubers_with_social_accounts = pio.read_json("plots/json/youtubers_count_with_social_accounts.json")
 channel_subs_per_country = pio.read_json("plots/json/channels_subs_per_country.json")
+
 
 EXERNAL_STYLESHEETS = ['assets/style.css']
 
@@ -17,11 +22,50 @@ app = Dash(__name__,
 
 
 app.layout = html.Div(children= [
-    html.Center(children= html.H2(children= "## Youtube gaming analysis")),
-    dcc.markdown("""this is sparta !!""")
+    
+    html.Center(children= html.H1(children= [html.Span(
+        "Youtube", style= {"color": "red"}), " Gaming Analysis"])),
+    
     dcc.Graph(
-        id='example-graph',
-        figure= channel_subs_per_country)])
+        id='channel_subs_per_country',
+        figure= channel_subs_per_country.update_layout(width= 610, height= 500),
+        style= {'position': 'absolute', 'border': '2px solid #fc0303','left': '10px',
+                'display': 'inline-block'}),
+    
+    dcc.Graph(
+        id= 'total_subs_vs_start_date',
+        figure= total_subs_vs_start_date.update_layout(width= 1230),
+        style= {'position': 'absolute', 'left': '10px', 'top': '600px', 'border': '2px solid #fc0303',
+                'display': 'inline-block'}),
+
+    dcc.Graph(
+        id= 'youtubers_with_social_accounts',
+        figure= youtubers_with_social_accounts.update_layout(width= 610, height= 500),
+        style= {'position': 'absolute','right': '10px', 'border': '2px solid #fc0303',
+                'display': 'inline-block'}),
+    
+    dcc.RangeSlider(0, 20, 1, value=[5, 15], id= 'subs-slider'),
+    
+    
+    html.Div(id='output-container-range-slider'),
+    
+    
+    dcc.Graph(
+        id= 'channels_subs_growth',
+        figure= channels_subs_growth.update_layout(width= 1230,
+                                                   xaxis= dict(rangeslider= dict(
+                                                   visible= True))),
+        style= {'position': 'absolute','left': '10px', 'border': '2px solid #fc0303', 'top': '1020px',
+                'display': 'inline-block'})])
+    
+    
+
+@app.callback(
+    Output('output-container-range-slider', 'children'),
+    [Input('my-range-slider', 'value')])
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
