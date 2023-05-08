5e8408ef6eaa8764db4ecb02874f7d4e2843ed47
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2023-05-08 09:04:27 +0300

    Semi finished the new CSS layout

--- Data analysis/app.py
+++ Data analysis/app.py
@@ -7,17 +7,17 @@ app = Dash(__name__, use_pages=True)
 
 
 for i, page in enumerate(dash.page_registry.values()):
-    
+
     if i == 0:
         channels_dashboard = page
-        
+
     elif i == 1:
         games_dashboard = page
-    
+
     elif i == 2:
         videos_dashboard = page
 
-        
+
 def create_button(dashboard: str) -> dcc.Link:
     """This function just help me to not repeat my code by
     creating customized buttons without rewriting the code."""
@@ -26,7 +26,7 @@ def create_button(dashboard: str) -> dcc.Link:
              href= f"{dashboard['relative_path']}",
              style= {"background-color": THEME_COLORS[1], "padding": "15px 25px",
                      "color": "white", "text-decoration": "none", "position": "relative",
-                     'border': '4px solid white', "top": "10px"})
+                     'border': '4px solid white', "top": "10px", "border-radius": "25px"})
     
     return  button
 
@@ -44,8 +44,8 @@ app.layout = html.Div([
     create_button(videos_dashboard),
                  
     # Page container to host the page output
-    html.Div([dash.page_container], style= {"top": "200px", 
-                                 "position": "relative"})])])
+    html.Div([dash.page_container], style= {"top": "300px", 
+                                            "position": "relative"})])])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- Data analysis/pages/.ipynb_checkpoints/videos_dashboard-checkpoint.py
+++ Data analysis/pages/.ipynb_checkpoints/videos_dashboard-checkpoint.py
@@ -24,6 +24,7 @@ EXERNAL_STYLESHEETS = ['assets/style.css']
 TODAY = datetime.now().strftime("%Y-%m-%d")
 
 DISTANCE: int = 5
+SPACE_DISTANCE = 0
 N_WIDTH: int = 600 # normal width
 L_WIDTH: int = (N_WIDTH * 2) + DISTANCE # large width
 
@@ -41,48 +42,74 @@ layout = html.Div(children= [
     html.Center(children= html.H2(children= [html.Span(
         "Gaming", style= {"color": THEME_COLORS[1]}), " Videos Dashboard"])),
     
+    
     dcc.Graph(
         id='videos_like_per_country',
         figure= videos_like_per_country.update_layout(width= N_WIDTH, height= N_HEIGHT),
+        
         style= {'position': 'relative', 'border': f'2px solid {THEME_COLORS[0]}',
-                'display': 'inline-block','left': f'{DISTANCE}px'},
+                'display': 'inline-block', "right" : f"{DISTANCE}px", "bottom": f"{N_HEIGHT + 2}px"},
+        
         config= {'displaylogo': False}),
+    
     
     dcc.Graph(
         id= 'social_accounts_affect_on_vid_stats',
-        figure= social_accounts_affect_on_vid_stats.update_layout(width= N_WIDTH, height= L_WIDTH),
+        figure= social_accounts_affect_on_vid_stats.update_layout(width= N_WIDTH,
+                                                                  height= L_HEIGHT + DISTANCE),
+        
         style= {'position': 'relative', 'border': f'2px solid {THEME_COLORS[0]}',
-                'display': 'inline-block', 'right': f'{DISTANCE}px'},
+                'display': 'inline-block', 'left': f'{DISTANCE * 2}px', 'top': f'{DISTANCE * 2}px'},
+        
         config= {'displaylogo': False}),
 
+    
     dcc.Graph(
         id= 'duration_vs_views',
-        figure= duration_vs_views.update_layout(width= N_WIDTH, height= N_HEIGHT),
-        style= {'position': 'relative', 'top': f'{N_HEIGHT + DISTANCE}px',
-                'display': 'inline-block', 'left': f'{DISTANCE}px', 'border': f'2px solid {THEME_COLORS[0]}'},
+        figure= duration_vs_views.update_layout(width= N_WIDTH,
+                                                height= N_HEIGHT - DISTANCE),
+        
+        style= {'position': 'relative', 'display': 'inline-block',
+                'bottom': f'{(N_HEIGHT + 2) - DISTANCE * 2}px', 'right': f'{100 + DISTANCE * 2}px',
+                'border': f'2px solid {THEME_COLORS[0]}'},
+        
         config= {'displaylogo': False}),
     
-        html.Div(["*Double click on legend buttons to isolate lines and bars."],
-             style= {'position': 'absolute', 'top': '1130px', 'height': '20',
-                'display': 'inline-block', 'left': '10px'}),
+    
+    html.Div(["*Double click on legend buttons to isolate lines and bars."],
+             style= {'position': 'relative', 'height': '20px', 'right': f'{L_WIDTH / 1.7}px',
+                     'bottom': f'{N_HEIGHT - (DISTANCE * 4)}px', 'display': 'inline-block'}),
+    
     
     dcc.Graph(
         id= 'video_stats_growth_per_time',
-        figure= video_stats_growth_per_time.update_layout(width= 1240, height= 500),
-        style= {'position': 'absolute', 'top': '1150px', 'border': f'2px solid {THEME_COLORS[0]}',
-                'display': 'inline-block', 'left': '10px'}, config= {'displaylogo': False}),
+        figure= video_stats_growth_per_time.update_layout(width= L_WIDTH + DISTANCE,
+                                                          height= N_HEIGHT),
+    
+        style= {'position': 'relative', 'bottom': f'{N_HEIGHT - (6 * DISTANCE)}px',
+                'border': f'2px solid {THEME_COLORS[0]}',
+                'display': 'inline-block', 'left': 'hi'},
+    
+        config= {'displaylogo': False}),
+
     
     dcc.Graph(
         id= 'video_stats_per_tag',
-        figure= video_stats_per_tag.update_layout(width= 1240, height= 500),
-        style= {'position': 'absolute', 'top': '1670px', 'border': f'2px solid {THEME_COLORS[0]}',
-                'display': 'inline-block', 'left': '10px'}, config= {'displaylogo': False}),
+        figure= video_stats_per_tag.update_layout(width= L_WIDTH + DISTANCE,
+                                                          height= N_HEIGHT),
     
-    dcc.Graph(
-        id= 'likes_vs_views',
-        figure= likes_vs_views.update_layout(width= 1240, height= 500),
-        style= {'position': 'absolute', 'top': '2190px', 'border': f'2px solid {THEME_COLORS[0]}',
-                'display': 'inline-block', 'left': '10px'}, config= {'displaylogo': False}),
+        style= {'position': 'relative', 'bottom': f'{N_HEIGHT + (DISTANCE)}px',
+                'border': f'2px solid {THEME_COLORS[0]}',
+                'display': 'inline-block', 'left': 'hi'},
+    
+        config= {'displaylogo': False}),
+    
+    
+# #     dcc.Graph(
+#         id= 'likes_vs_views',
+#         figure= likes_vs_views.update_layout(width= 1240, height= 500),
+#         style= {'position': 'absolute', 'top': '2190px', 'border': f'2px solid {THEME_COLORS[0]}',
+#                 'display': 'inline-block', 'left': '10px'}, config= {'displaylogo': False}),
     
     html.Div(["By: Muhammed Ahmed Abd-Al-Aleam Elsayegh", html.Br(),
                  f"Last update: {TODAY}"],
