83cb4d791aacdeeadd76c1597bd0927304289e79
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2023-05-25 10:19:28 +0300

    Worked on the App functionallity and some bugs fixing

--- ML & NLP analysis/app.py
+++ ML & NLP analysis/app.py
@@ -5,6 +5,22 @@ import keras
 import pickle
 import pandas as pd
 import numpy as np
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+from googleapiclient.discovery import build
+import googleapiclient.errors
+# ============================================================
+
+
+
+# ===================Settting up YouTube build================
+API_KEY: str = "AIzaSyANcOOmvv5fs6Gx7vKXucSelmScjx3V3Qg"
+API_SERVICE_NAME = "youtube"
+API_VERSION = "v3"
+
+
+youtube = build(
+    API_SERVICE_NAME, API_VERSION, developerKey= API_KEY)
 # ============================================================
 
 
@@ -39,6 +55,7 @@ for processor_name, processor in preprocessors.items():
 
 # ==================Getting user interacts====================
 def local_css(file_name):
+    
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
         
@@ -67,18 +84,131 @@ with st.sidebar:
     add_radio = st.radio(
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)"))
+# ============================================================
+
+
+
+# ==============Gettign the channel & videos data=============
+
+# Getting the channel's id
+channel_id_response = youtube.search().list(
+    q= channel_name,
+    type='channel',
+    part='id',
+    maxResults= 1
+).execute()
+
+channel_id = channel_id_response['items'][0]['id']['channelId']
+
+
+# Getting channel's stats
+channel_stats_response = youtube.channels().list(
+        part= "snippet,contentDetails,statistics",
+        id= channel_id).execute()
+
+item = channel_stats_response["items"][0]
+
+channel_stats = pd.DataFrame({"channel_name": item["snippet"]["title"],
+                              "subscribers": item["statistics"]["subscriberCount"],
+                              "playlist_id": item["contentDetails"]["relatedPlaylists"]["uploads"],
+                              "total_views": item["statistics"]["viewCount"],
+                              "start_date": item["snippet"]["publishedAt"],
+                              "video_count": item["statistics"]["videoCount"],
+                              "about": item["snippet"]["description"]}
+                              , index=[0])
+
+
+# Loading 50 videos from the channel
+max_results = [50]
     
+def get_videos_stats(playlist_id):
+
+    # nonlocal max_results
+    
+    videos_ids_request = youtube.playlistItems().list(
+        part= "snippet,contentDetails",
+        playlistId= playlist_id,
+        maxResults= max_results[0])
+
+    try:
+        videos_ids_response = videos_ids_request.execute()
+        videos_ids = videos_ids_response["contentDetails"]["videoId"]
+
+    except:
+        max_results[0] = channel_stats["video_count"][0]
+        get_videos_stats(playlist_id)
+        
+    
+videos_ids = get_videos_stats(channel_stats["playlist_id"][0])
+
+
+# Loading the videos stats
+def get_video_stats(videos_ids: list) -> pd.DataFrame:
+
+    """This function takes the videos IDs list and request
+       for the statistics of the videos then save them into
+       a DataFrame."""
+
+    all_video_info = []
+    thumbnails = []
+    videos_count = len(videos_ids)
+
+    for i in range(0, videos_count, 50):
+        
+        chunk = videos_ids[i:i+50]
+        processed_videos_count = i + len(chunk)
+        
+        # Giving the request for each 50 video in one time
+        request = youtube.videos().list(
+            part="snippet,contentDetails,statistics",
+            id=','.join(chunk))
+        response = request.execute()
+        
+        # Calculate the progress with updating it.
+        print(f"Finished {processed_videos_count / videos_count * 100:.2f}% of loading the videos data")
+        clear_output(wait= True)
+        time.sleep(0.00001)
+
+        for video in response['items']:
+            video_json_encoder = {"snippet": ['channelTitle', 'title', 'description', 'tags', 'publishedAt'],
+                                  "statistics": ['viewCount', 'likeCount', 'commentCount'],
+                                  "contentDetails": ["duration", "definition"]}
+
+            video_info = {}
+            video_info['video_id'] = video['id']
+
+            for key in video_json_encoder.keys():
+                for val in video_json_encoder[key]:
+                    try:
+                        video_info[val] = video[key][val]
+                    except:
+                        video_info[val] = np.nan
+
+            all_video_info.append(video_info)
+
+    df = pd.DataFrame(all_video_info)
+    
+    return df
+
+videos_stats = get_video_stats(videos_ids)
+# ============================================================
+
+
+
+# ==================Feature engineering=======================
 
 # ============================================================
 
 
 
-# ==================Returning basic outputs===================
+# ====================Updatibg the output=====================
 
+st.text_box(channel_id)
 # ============================================================
-
 option = st.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone'))
 
 st.write('You selected:', option)
+
+print(channel_id)
