66d8e5dd01f856335dc97a123de8df88da7bb4c4
muhammed-abdelaleam <muhammed.abdelaleam@gmail.com>
2024-05-01 12:20:06 +0300

    worked on the channels mining

--- mining/mining.py
+++ mining/mining.py
@@ -9,59 +9,81 @@ following querys:
 
 And finally all of those data will be stored in the main database with a backup
 for raw-data.
+
+Note: some functions in this code is imported by update.py
 """
 
 import os
 import pandas as pd
 import sqlite3
 import requests
+import itertools
 import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 
 MAX_RESULTS: int = 50
-# TODO upscale channels count (have to be doubler to MAX_RESULTS)
 CHANNELS_COUNT: int = 100
-VIDEOS_PER_CHANNEL: int = 10  # TODO upscale videos per channel
+VIDEOS_PER_CHANNEL: int = 10
 
 SEARCH_QUERY = "gaming"
 API_KEY = os.getenv('YOUTUBE_API')
 
 
-def search_channels() -> set[str]:
+# helper function
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+
+def search_channels(save_page_token= False) -> set[str]:
 	"""
 	Returns a set of channels ids knowing the API_KEY and SEARCH_QUERY global
-	constants without needing any param.
+	constants also this function save pagetoken using bool param sat to False
+	by defualt to create a txt file and save the next page token to continue
+	the search from where we stopped on deffrint days.
 
-	@params: None
+	@params: save_page_token and load_page_token bool params
 	@returns: set of channels ids as strings
 	"""
 
 	url = 'https://www.googleapis.com/youtube/v3/search'
 	params = {
-	    "key": API_KEY,
-	    "part": "snippet",
-	    "q": SEARCH_QUERY,
-	    "type": "channel",
-	    "maxResults": MAX_RESULTS
+		"key": API_KEY,
+		"part": "snippet",
+		"q": SEARCH_QUERY,
+		"type": "channel",
+		"maxResults": MAX_RESULTS
 	}
+
+	if os.path.exists('pagetoken.txt'):
+		with open('pagetoken.txt', 'r') as page_token:
+			params['pageToken'] = page_token.read()
 
 	channels_ids = set()
 
 	for _ in range(int(CHANNELS_COUNT / MAX_RESULTS)):
 
 		response = requests.get(url, params=params)
+		response.raise_for_status()
 
-		if response.status_code == 200:
-			data = response.json()
+		data = response.json()
 
-			for channel in data['items']:
-				channels_ids.add(channel['id']['channelId'])
-				next_page_token = data.get("nextPageToken")
+		for channel in data['items']:
+			channels_ids.add(channel['id']['channelId'])
+			next_page_token = data.get("nextPageToken")
 
-				if next_page_token:
-					params['pageToken'] = next_page_token
+			if next_page_token:
+				params['pageToken'] = next_page_token
+
+			else:
+				# this condition should never execute but just in case
+				next_page_token = ''
+				raise Exception('Scraped all avelaible gaming channels')
+
+	if save_page_token:
+		with open('pagetoken.txt', 'w') as f:
+			f.write(next_page_token)
 
 	return channels_ids
 
@@ -76,8 +98,40 @@ def get_channels_data(channels_ids: set[str]) -> pd.DataFrame:
 	@returns: a pandas dataframe with the channel stats and data
 	"""
 
-	url = 'https://www.googleapis.com/youtube/v3/channels'
-	raise NotImplementedError
+	df = pd.DataFrame(columns= ['channel_name', 'subscribers', 'total_views',
+		'date', 'playlist_id', 'video_count', 'about'])
+
+	for channel_ids_block in chunker(list(channels_ids), MAX_RESULTS):
+
+		print(channel_ids_block)
+
+		url = 'https://www.googleapis.com/youtube/v3/channels'
+		params = {
+			"key": API_KEY,
+			"part": "snippet,statistics,contentDetails",
+			"id": ','.join(channel_ids_block),
+			"maxResults": MAX_RESULTS
+		}
+
+		response = requests.get(url, params=params)
+		response.raise_for_status()
+
+		data = response.json()
+		print(data)
+		for channel in data['items']:
+			channel_data = {
+					"channel_name":     channel["snippet"]["title"],
+					"subscribers":      channel["statistics"]["subscriberCount"],
+					"total_views":      channel["statistics"]["viewCount"],
+					"date":             channel["snippet"]["publishedAt"],
+					"playlist_id":      channel["contentDetails"]["relatedPlaylists"]["uploads"],
+					"video_count":      channel["statistics"]["videoCount"],
+					"about":            channel["snippet"]["description"]
+				}
+
+		df.append(channel_data, ignore_index=True) # tofix
+
+	return df
 
 
 def get_videos(playlist_ids: list) -> list:
@@ -87,10 +141,15 @@ def get_videos(playlist_ids: list) -> list:
 def get_videos_data(videos_ids: list) -> pd.DataFrame:
 	raise NotImplementedError
 
-
+# testing only
 def main():
-	channels_ids = search_channels()
-	print('Channels ids: ', channels_ids)
+
+	# channels_ids: set = search_channels()
+
+	with open('../data/channels-ids.txt', 'r') as f:
+		channels_ids = eval(f.read())
+
+	df = get_channels_data(channels_ids)
 
 if __name__ == '__main__':
 	main()
